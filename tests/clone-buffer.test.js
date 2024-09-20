5418f97d205aa0b58154409cd72bef9b4506422a
Jesús Seijas <jseijas@gmail.com>
2024-09-20 21:29:57 +0200

    first commit

---
+++ tests/clone-buffer.test.js
@@ -0,0 +1,10 @@
+const { expect, cloneBuffer, describe, it } = require('../lib');
+
+describe('Clone Buffer', () => {
+  it('Should clone a buffer', () => {
+    const buffer = Buffer.from('123456789abcdefghi');
+    const actual = cloneBuffer(buffer);
+    expect(actual).toNotBe(buffer);
+    expect(actual.toString()).toEqual(buffer.toString());
+  });
+});
